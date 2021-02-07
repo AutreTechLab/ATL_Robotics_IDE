15107d88f9febc2f60d433dbf1e78b0b08f4f2c6
AutreTechLab <autretechlab@gmail.com>
2021-02-07 15:18:55 +0100

    Small changes

--- ros2_ws/env.bash
+++ ros2_ws/env.bash
@@ -1,4 +1,4 @@
 source /opt/ros/foxy/setup.bash
 #export ROS2_WEBOTS_HOME="/usr/local/webots"
 export WEBOTS_HOME="/usr/local/webots"
-source ~/Documents/GitHub/ATL_Robotics_IDE/ros2_ws/install/local_setup.bash
+source ./install/local_setup.bash
